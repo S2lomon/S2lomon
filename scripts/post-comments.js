501ebad93c04ef587dc5fc1c1f95e3e102e3a7dc
Szymon Homa <s2lomon@gmail.com>
2016-09-27 23:03:25 +0200

    improving comments section

--- scripts/post-comments.js
+++ scripts/post-comments.js
@@ -1,12 +1,18 @@
     var loadComments = function (issueId, data) {
+        var commentsElement = jQuery("#comments");
+        commentsElement.empty();
         for (var i=0; i<data.length; i++) {
             var cuser = data[i].user.login;
             var cuserlink = "https://www.github.com/" + data[i].user.login;
             var clink = "https://github.com/s2lomon/s2lomon/issues/"+issueId+"#issuecomment-" + data[i].url.substring(data[i].url.lastIndexOf("/")+1);
             var cbody = data[i].body_html;
             var cavatarlink = data[i].user.avatar_url;
-            var cdate = data[i].created_at;
-            jQuery("#comments").append(
+            var creation_date = data[i].created_at;
+            var cdate = creation_date
+                .replace('T', ' ')
+                .replace('Z', ' ');
+
+            commentsElement.append(
             "<div class='comment'>"+
                 "<div class='comment-header'>" +
                     '<img src="' + cavatarlink + '" alt="avatar">'+
@@ -36,6 +42,23 @@
     var handleComments = function (issueId) {
         requestsComments(issueId, loadComments, logError);
     };
+
+    var prepareCommentsToogle = function(toggleTrigger, toggleTarget) {
+        jQuery(toggleTrigger).on("click", () => {
+            toggleContent(toggleTarget);
+        })
+    }
+
+    var prepareCommentsRefresh = function(refreshTrigger, issueId) {
+        jQuery(refreshTrigger).on('click', () => handleComments(issueId));
+    }
+
+    var COMMENTS_TOGGLE_ANIMATION_TIME = 200;
+    var toggleContent = function (target) {
+        var target = jQuery(target);
+        target.toggleClass('no-height', COMMENTS_TOGGLE_ANIMATION_TIME );
+    }
+
 
 
 
